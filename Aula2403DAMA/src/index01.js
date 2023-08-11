8b004098ec04279fa8fb624d2466d1a7fe237115
lauracabrall <109156214+lauracabrall@users.noreply.github.com>
2023-08-11 12:10:07 -0300

    Delete Aula2403DAMA directory

--- Aula2403DAMA/src/index01.js
+++
@@ -1,9 +0,0 @@
-const prompt = require(`prompt-sync`)()
-
-let salarioBruto = parseFloat(prompt(`Digite o salário bruto do contribuinte: `))
-let previdencia = salarioBruto * 0.1
-let salarioApos = salarioBruto - previdencia
-let imposto = salarioApos * 0.05
-let salarioLiquido = salarioApos - imposto
-
-console.log(`O salário líquido do contribuinte é: ${salarioLiquido.toFixed(2)}`)
--- Aula2403DAMA/src/index05.js
+++
@@ -1,9 +0,0 @@
-const prompt = require(`prompt-sync`)()
-
-const vetor = [321, 534, 123]
-
-vetor.sort((a, b) => {
-    return a - b
-})
-
-console.log(vetor)
